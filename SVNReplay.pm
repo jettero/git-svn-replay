835575c71849a0837e3d5a43df809b705d186ad4
Paul Miller <jettero@cpan.org>
2009-04-24 11:26:41 -0400

    must chdir into the svn co *first* for this logic to work

--- SVNReplay.pm
+++ SVNReplay.pm
@@ -267,11 +267,11 @@ sub add_svn_dir {
     my ($this, $cod) = @_;
 
     my $co = File::Spec->rel2abs( $this->{svn_co} );
+    chdir $co or croak "couldn't chdir into svn_co ($this->{svn_co}): $!";
+
     my $r  = File::Spec->rel2abs( $cod );
        $r =~ s/^\Q$co\E\///
            or croak "$cod doesn't want to be located under $this->{svn_co}";
-
-    chdir $co or croak "couldn't chdir into svn_co ($this->{svn_co}): $!";
 
     unless( -d $r ) {
         ebegin "adding $cod to $this->{svn_co}";
